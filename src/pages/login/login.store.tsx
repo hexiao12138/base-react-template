60cf773fd27de6e46d6f5f6e9cc7b022a40b3314
hexiao <hexiao12138@163.com>
2023-12-23 16:46:10 +0800

    feat: 初始化项目

--- src/pages/login/login.store.tsx
+++ src/pages/login/login.store.tsx
@@ -1,11 +1,4 @@
-import { Store } from 'antd/lib/form/interface';
-import { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-// import { LoginParam } from '~/modal/common/common.interface';
-// import { commonService } from '~/modal/common/common.service';
-// import { useRememberMe } from '~/stores/remember-me';
-// import { useUserInfoStore } from '~/stores/user-info';
-import { message } from 'antd';
+
 export function useLoginState() {
 
 
